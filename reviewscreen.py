2e6f18115456f4ab6e109d2218b255881c0065c6
eriksandberg <eriksandbergum@gmail.com>
2019-12-14 18:50:07 -0600

    Added menu in the review screen to select a pickup location.

--- reviewscreen.py
+++ reviewscreen.py
@@ -6,12 +6,20 @@ from kivymd.uix.card import MDCard
 class ReviewCard(MDCard):
     pass
 
+
 from kivy.animation import Animation
 from kivy.metrics import dp
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, StringProperty
+from kivymd.uix.dialog import ListMDDialog
+
+from kivymd.uix.list import TwoLineListItem
+class LocationListItem(TwoLineListItem):
+    pass
 
 class ReviewScreen(Screen):
     a = ObjectProperty(None)
+    location_popup = ObjectProperty(None)
+    location = StringProperty("") # Address of location to pick up from
 
     def animate_fab(self, *args):
         fab = self.ids.fab
@@ -48,3 +56,33 @@ class ReviewScreen(Screen):
             self.ids.card_list.add_widget(card)
             total_label.text = str(float(total_label.text) + card.cost)
         self.ids.card_list.add_widget(ReviewCard(opacity=0))
+
+    def display_choose_location_menu(self):#, item_screen):
+        self.location_popup = ListMDDialog()
+        self.location_popup.title = "Select a location"
+        self.location_popup.size_hint = (.8, .8)
+        locations = self.get_locations()
+        for location in locations:
+            location = LocationListItem(text=location[0], secondary_text=location[1])
+            if location.parent:
+                location.parent.remove_widget(location)
+            location.dialog = self.location_popup
+            #location.item_screen = item_screen
+            location.screen = self
+            self.location_popup.ids.list_layout.add_widget(location)
+        self.location_popup.open()
+
+    def get_locations(self):
+        """Get locations from some kind of online database.
+
+        :return:
+        """
+        locations = [["182 W Ridley Pl.", self.distance_to_location(0,0)+ " miles away"]]
+        locations *= 3
+        return locations
+
+
+    def distance_to_location(self, lat, lon):
+        """Calculate distance in miles from user's phone's GPS to restaurant
+        location."""
+        return "3.12"
